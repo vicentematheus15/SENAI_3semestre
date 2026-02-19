36ae5a3c0878134a09592811dc556c380df04944
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-02-19 20:53:51 -0300

    Fix variable name for values extraction in filme object and add console output

---
+++ revisao/dados/exercicios/2.2.js
@@ -0,0 +1,16 @@
+// Exercício 2.2 - Extração de Dados
+// Dado o objeto const filme = { titulo: "Matrix", diretor: "Wachowskis", lancamento: 1999 };:
+// Crie uma variável contendo apenas as chaves desse objeto e exiba no console.
+// Crie uma variável contendo apenas os valores desse objeto e exiba no console.
+
+const filme = {
+    titulo: "Matrix",
+    diretor: "Wachoskis",
+    lancamento: 1999
+};
+
+const chaves = Object.keys(filme);
+console.log(chaves);
+
+const valores = Object.keys(filme);
+console.log(vlaores);
