63922a4c669f68b1e43da33331e0f89742c5fd25
Matheus Vicente <vicentematheus1507@gmail.com>
2026-02-18 21:38:22 -0300

    Add implementation of Veiculo class with Carro and Aviao subclasses for vehicle movement

---
+++ revisao/poo/exercicios/07.js
@@ -0,0 +1,46 @@
+// Crie uma classe base Veiculo com métodos como mover(). Crie subclasses Carro e Aviao. O carro deve mover-se "pela estrada" e o avião "pelo ar". Adicione atributos específicos, como quantidadePortas para o carro e altitudeMaxima para o avião.
+
+// Classe base
+class Veiculo {
+  constructor(nome) {
+    this.nome = nome;
+  }
+
+  mover() {
+    console.log("O veículo está se movendo.");
+  }
+}
+
+// Subclasse Carro
+class Carro extends Veiculo {
+  constructor(nome, quantidadePortas) {
+    super(nome);
+    this.quantidadePortas = quantidadePortas;
+  }
+
+  mover() {
+    console.log(this.nome + " está se movendo pela estrada.");
+  }
+}
+
+// Subclasse Aviao
+class Aviao extends Veiculo {
+  constructor(nome, altitudeMaxima) {
+    super(nome);
+    this.altitudeMaxima = altitudeMaxima;
+  }
+
+  mover() {
+    console.log(this.nome + " está se movendo pelo ar.");
+  }
+}
+
+// Teste
+const carro = new Carro("Fusca", 2);
+const aviao = new Aviao("Boeing", 12000);
+
+carro.mover(); // pela estrada
+aviao.mover(); // pelo ar
+
+console.log("Portas do carro:", carro.quantidadePortas);
+console.log("Altitude máxima do avião:", aviao.altitudeMaxima);
