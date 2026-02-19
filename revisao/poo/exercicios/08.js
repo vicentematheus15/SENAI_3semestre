9140622ad332aa09082cc2dcbd9011dbfdc33fef
Matheus Vicente <vicentematheus1507@gmail.com>
2026-02-18 21:39:05 -0300

    Add implementation of Funcionario and Gerente classes with method to display employee details

---
+++ revisao/poo/exercicios/08.js
@@ -0,0 +1,33 @@
+// Crie uma classe Funcionario com salário base. Crie uma subclasse Gerente que herda de Funcionário. No construtor do Gerente, utilize super() para repassar dados e adicione um atributo extra departamento. Sobrescreva um método de exibir dados para incluir o departamento.
+
+
+// Classe base
+class Funcionario {
+  constructor(nome, salarioBase) {
+    this.nome = nome;
+    this.salarioBase = salarioBase;
+  }
+
+  exibirDados() {
+    console.log("Nome:", this.nome);
+    console.log("Salário:", this.salarioBase);
+  }
+}
+
+// Subclasse Gerente
+class Gerente extends Funcionario {
+  constructor(nome, salarioBase, departamento) {
+    super(nome, salarioBase); // repassa dados para a classe Funcionario
+    this.departamento = departamento;
+  }
+
+  // Sobrescrevendo o método
+  exibirDados() {
+    super.exibirDados(); // chama o método da classe base
+    console.log("Departamento:", this.departamento);
+  }
+}
+
+// Teste
+const gerente = new Gerente("Carlos", 8000, "TI");
+gerente.exibirDados();
