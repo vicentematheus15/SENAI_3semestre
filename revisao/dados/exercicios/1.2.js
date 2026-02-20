7bbab5b581d88e8e30b48540f0f66b7cd2876434
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-02-19 21:01:03 -0300

    Add inventory management example with item count, specific item access, and item removal

---
+++ revisao/dados/exercicios/1.2.js
@@ -0,0 +1,14 @@
+// Dado o array let inventario = ["Espada", "Escudo", "Poção de Vida", "Mapa"];:
+// Exiba no console a quantidade total de itens no inventário.
+// Acesse e exiba no console apenas o item "Poção de Vida" utilizando seu índice.
+// Remova o último item do inventário e guarde-o em uma variável chamada itemPerdido.
+
+let inventario = ["Espada", "Escudo", "Poção de Vida", "Mapa"];
+
+console.log(inventario.length);
+
+console.log(inventario[2]);
+
+const itemPerdido = console.log(inventario.pop())
+
+
