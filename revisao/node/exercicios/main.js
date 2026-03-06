e16523e3bfd67f0168113ef1594e7297980540d9
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-03-05 22:08:03 -0300

    Implement log file reader module to count and display lines using fs/promises

--- revisao/node/exercicios/main.js
+++ revisao/node/exercicios/main.js
@@ -1,14 +1,6 @@
 // Exercício 1 — Leitor de Logs
 // Crie um módulo que leia um arquivo log.txt utilizando fs/promises. O programa deve contar quantas linhas existem no arquivo e exibir o total no console. Utilize import ao invés de require.
-import {readFile} from 'fs/promises';
 
-async function leitorArquivo() {
-    try{
-        const arquivo = await readFile('./log.txt', 'utf-8');
-        console.log(typeof(arquivo));
-    }catch(err){
-        console.log("deu erro:", err)
-    }
-}
+import { leitorArquivo } from './contador.js';
 
 leitorArquivo()
