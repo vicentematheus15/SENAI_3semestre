1fce1d281d4d73715ddac1ccfbfbbdd1451ef9e8
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-06 16:13:29 -0300

    Refactor report generation script to improve data handling and create relatorio.txt

--- revisao/node/exercicios/ex02/script.js
+++ revisao/node/exercicios/ex02/script.js
@@ -4,30 +4,35 @@ import {readFile, writeFile} from 'fs/promises'
 
 async function lerArquivo(){
     try{
-        const arquivoJson = await readFile('./dados.json', 'utf-8');
-        console.log(arquivoJson);
+        const dadosBrutos = await readFile('./dados.json', 'utf-8');
+        return dadosBrutos
     }catch(err){
         console.log("DEU ERRO:", err)
     }
 }
 
-async function formatarArquivo(arquivoJson){
-    const arquivoFormatado = JSON.stringify(arquivoJson)
-    console.log("ok");
-    
-    return arquivoFormatado
+async function formatarArquivo(dadosBrutos){
+    const objetoDados = JSON.parse(dadosBrutos)
+    const dadosFormatados = `O livro ${objetoDados.livro} foi escrito por ${objetoDados.autor}`
+    console.log(dadosFormatados)
+    return dadosFormatados
 }
 
-async function gerarRelatorio(arquivoFormatado) {
+async function gerarRelatorio(dadosFormatados) {
     try{
-        const relatorio = await writeFile('./relatorio.txt', arquivoFormatado)
-        console.log(relatorio);
+        const relatorio = await writeFile('./relatorio.txt', dadosFormatados)
     }catch(err){
         console.log("deu erro:", err);
         
     }
 }
+async function main() {
+    
+    const dadosBrutos = await lerArquivo();
+    const textoFormatado = await formatarArquivo(dadosBrutos);
+    await gerarRelatorio(textoFormatado);
+    console.log("Processo finalizado");
+    
+}
 
-lerArquivo();
-formatarArquivo();
-gerarRelatorio();
+main();
