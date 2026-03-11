c11ef518773d113c5135336923496e978169339d
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-11 20:35:54 -0300

    feat: Add script to read a text file and display line and word counts

--- Desenvolvimento Backend/node/Modularizacao/lista1 - core modules (fs, os e path)/ex04/script.js
+++ Desenvolvimento Backend/node/Modularizacao/lista1 - core modules (fs, os e path)/ex04/script.js
@@ -2,3 +2,11 @@
 
 import {readFile} from 'fs/promises';
 
+async function lerArquivo(){
+    const conteudo = await readFile('./texto.txt', 'utf-8')
+    const qtdLinhas = conteudo.trim().split('\n')
+    const qtdPalavras = conteudo.split(' ')
+    console.log(`Número de linhas: ${qtdLinhas.length} \nNúmero de palavras: ${qtdPalavras.length} `);
+}
+
+const dados = await lerArquivo()
