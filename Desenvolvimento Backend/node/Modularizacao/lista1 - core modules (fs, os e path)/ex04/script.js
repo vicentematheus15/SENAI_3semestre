e09bcce3e39ec0010e805683c6ae5f4870f6c565
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-11 20:43:38 -0300

    fix: Update word count calculation and include character count in output

--- Desenvolvimento Backend/node/Modularizacao/lista1 - core modules (fs, os e path)/ex04/script.js
+++ Desenvolvimento Backend/node/Modularizacao/lista1 - core modules (fs, os e path)/ex04/script.js
@@ -5,8 +5,8 @@ import {readFile} from 'fs/promises';
 async function lerArquivo(){
     const conteudo = await readFile('./texto.txt', 'utf-8')
     const qtdLinhas = conteudo.trim().split('\n')
-    const qtdPalavras = conteudo.split(' ')
-    console.log(`Número de linhas: ${qtdLinhas.length} \nNúmero de palavras: ${qtdPalavras.length} `);
+    const qtdPalavras = conteudo.split(/\s+/)
+    console.log(`Número de linhas: ${qtdLinhas.length} \nNúmero de palavras: ${qtdPalavras.length} \nNúmero de caracteres: ${conteudo.length}`);
 }
 
 const dados = await lerArquivo()
