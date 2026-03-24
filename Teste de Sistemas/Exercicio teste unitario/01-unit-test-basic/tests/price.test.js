0069c892871b6112ba2bda1e54f21776db977d73
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-23 22:35:05 -0300

    feat: add unit tests for calculateTotal function in price module

---
+++ Teste de Sistemas/Exercicio teste unitario/01-unit-test-basic/tests/price.test.js
@@ -0,0 +1,56 @@
+import {describe, test, expect} from "vitest";
+import { calculateTotal } from "../src/price";
+
+describe("calculateTotal()", () => {
+    test("Deve retornar o total correto para os valores válidos", () => {
+        // Arrange
+        const price = 10;
+        const quantity = 3;
+
+        //Act
+        const result = calculateTotal(price, quantity);
+
+        //Assert
+        expect(result).toBe(30);
+
+
+    })
+
+    test("O resultado deve ser do tipo number", () => {
+        //Arrange
+        const price = 5
+        const quantity = 2
+
+        //Act
+        const result = calculateTotal(price, quantity);
+
+        //Assert
+        expect(typeof result).toBe("number");
+        expect(result).toBeTypeOf("number");
+    })
+
+    test("O resultado nao deve ser NaN", () => {
+        // Arrange
+        const price = 9.99
+        const quantity = 3
+
+        //Act
+        const result = calculateTotal(price, quantity)
+
+        //Assert
+        expect(result).not.toBeNaN();
+    })
+
+    test("Deve lancar um erro quando o preco e zero", () => {
+        // Arrange
+        const price = 0;
+        const quantity = 5;
+
+        //Act
+        const result = () => calculateTotal(price, quantity);
+
+        //Assert
+        expect(result).toThrow()
+
+    })
+})
