74ca74ed22755d73a0d6462281c845e02f69ccc1
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-03-30 20:15:17 -0300

    feat: implement basic arithmetic functions and corresponding unit tests for add, subtract, multiply, and divide

--- Teste de Sistemas/Exercicio teste unitario/02-calc-project/src/coreMath.js
+++ Teste de Sistemas/Exercicio teste unitario/02-calc-project/src/coreMath.js
@@ -11,10 +11,27 @@
  *    parâmetro for zero.
  */
 
-export function add(a, b) {}
+export function add(a, b) {
+    return a + b
+}
 
-export function subtract(a, b) {}
+export function subtract(a, b) {
+    return a - b
+}
 
-export function multiply(a, b) {}
+export function multiply(a, b) {
+    return a * b
+}
 
-export function divide(a, b) {}
+export function divide(a, b) {
+    if(b === 0){
+        throw new Error("Não é possível dividir por 0!");
+    }
+    return a / b
+}
+
+// console.log(add(2, -3))
+// console.log(subtract(6, 12));
+// console.log(multiply(2, -5));
+// console.log(divide(9, 1));
+
--- Teste de Sistemas/Exercicio teste unitario/02-calc-project/tests/coreMath.test.js
+++ Teste de Sistemas/Exercicio teste unitario/02-calc-project/tests/coreMath.test.js
@@ -24,3 +24,58 @@ import { describe, it, expect } from 'vitest';
 import { add, subtract, multiply, divide } from '../src/coreMath.js';
 
 // escreva seus testes aqui
+describe('add()', () => {
+    it('Retornar a soma de dois valores positivos', () => {
+        expect(add(5, 4)).toBe(9)
+    })
+
+    it('Retornar a soma com um parâmetro negativo', () => {
+        expect(add(6, -8)).toBe(-2)
+    })
+
+    it('Rtornar a soma com uma parâmetro sendo 0', () => {
+        expect(add(0, 2)).toBe(2)
+    })
+})
+
+describe('subtract()', () => {
+    it('Retornar a subtração de dois valores', () => {
+        expect(subtract(9, 3)).toBe(6)
+    })
+
+    it('Retornar um resultado negaivo (subtraendo maior que minuendo)', () => {
+        expect(subtract(12, 16)).toBe(-4)
+    })
+
+    it('Retornar uma subtração com um parâmetro sendo 0', () => {
+        expect(subtract(7, 0)).toBe(7)
+    })
+})
+
+describe('multply()', () => {
+    it('Retorna a multiiplicação de dois valores positivos', () => {
+        expect(multiply(2, 5)).toBe(10)
+    })
+
+    it('Retorna uma multiplicação com um valor sendo 0', () => {
+        expect(multiply(6, 0)).toBe(0)
+    })
+
+    it('Retorna um resultado negativo', () => {
+        expect(multiply(-3, 5)).toBe(-15)
+    })
+})
+
+describe('divide()', () => {
+    it('Retorna um divisão exata', () => {
+        expect(divide(100, 20)).toBe(5)
+    })
+
+    it('Retorna um resultado decimal', () => {
+        expect(divide(60, 5)).toBe(12.5)
+    })
+
+    it('Deve retornar um erro ao tentar dividir por 0', () => {
+        expect(divide(18, 0)).toThrow("Não é possível dividir por 0!")
+    })
+})
