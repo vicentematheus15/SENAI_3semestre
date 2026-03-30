2b21e51846c69ee0db500ede71974f6db4d761c9
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-30 15:49:16 -0300

    feat: add unit tests for calculateShipping function; include boundary value checks and error handling for zero weight

--- Teste de Sistemas/Exercicio teste unitario/01-unit-test-basic/tests/shipping.test.js
+++ Teste de Sistemas/Exercicio teste unitario/01-unit-test-basic/tests/shipping.test.js
@@ -1,9 +1,60 @@
-// describe("", () => {
-//     test("", () => {
-//         //Arrange
+import {describe, test, expect} from 'vitest';
+import { calculateShipping } from '../src/shipping';
 
-//         //Act
+describe("calculateShipping()", () => {
+    test("Retorna 10 para peso até 1kg", () => {
+        //Arrange
+        const weight = 0.8;
 
-//         //Assert
-//     })
-// })
+        //Act
+        const result = calculateShipping(weight);
+        
+        //Assert
+        expect(result).toBe(10)
+    })
+
+    test("Retorna 20 para peso entre 1kg e 5kg", () => {
+        //Arrange
+        const weight = 3;
+
+        //Act
+        const result = calculateShipping(weight);
+
+        //Assert
+        expect(result).toBe(20)
+    })
+
+    test("Retorna 40 para peso acima de 5kg", () => {
+        //Arrange
+        const weight = 6;
+
+        //Act
+        const result = calculateShipping(weight);
+
+        //Assert
+        expect(result).toBe(40)
+    })
+
+    test("Testa limites (boundary values: 5kg", () => {
+        //Arrange
+        const weight = 5
+
+        //Act
+        const result = calculateShipping(weight);
+
+        //Assert
+        expect(result).toBe(20)
+
+    })
+
+    test("Retorna erro quando o peso é 0", () => {
+        //Arrange
+        const weight = 0;
+
+        //Act
+        const result = () => calculateShipping(weight);
+
+        //Assert
+        expect(result).toThrow("Peso inválido")
+    })
+})
