4f46d5fe85a3b04a78a020eed44ef47b0731772e
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-30 14:47:06 -0300

    feat: add additional test cases for product validation; handle empty name and zero stock scenarios

--- Teste de Sistemas/Exercicio teste unitario/01-unit-test-basic/tests/product.test.js
+++ Teste de Sistemas/Exercicio teste unitario/01-unit-test-basic/tests/product.test.js
@@ -32,5 +32,44 @@ describe("validateProduct()", () => {
         //da pra juntar o act e o expect mandando diretamente a arrow function do validateProduct para dentro do expect em vez de guardar ela em uam variável antes, assim:
         //expect(() => validateProduct(product)).toThrow("O preço não pode ser negativo")
     })
+
+    test("Deve retornar um erro ao inserir estoque negativo", () => {
+        //Arrange
+        const product = {
+            name: "Sorvete",
+            price: 8,
+            stock: -5
+        }
+        //Act
+        const result = () => validateProduct(product)
+        //Assert
+        expect(result).toThrow("Estoque inválido")
+    })
+
+    test("Deve aceitar produtos com 0 de estoque", () => {
+        //Arrange
+        const product = {
+            name: "Camisa",
+            price: 10,
+            stock: 0
+        }
+        //Act
+        const result = validateProduct(product)
+        //Assert
+        expect(result).toBeTruthy()
+    })
+
+    test("Deve retornar um erro caso o campo 'nome' fique vazio", () => {
+        //Arrange
+        const product = {
+            name: "",
+            price: 20,
+            stock: 30
+        }
+        //Act
+        const result = () => validateProduct(product)
+        //Assert
+        expect(result).toThrow("Nome obrigatório")
+    })
 })
 
