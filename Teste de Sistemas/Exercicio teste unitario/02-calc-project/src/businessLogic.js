cf4353b6f745c9d674f08da043179aa77e726f70
Matheus Vicente <vicentematheus1507@gmail.com>
2026-03-30 21:34:39 -0300

    feat: implement calculateDiscountedPrice function with input validation and error handling

--- Teste de Sistemas/Exercicio teste unitario/02-calc-project/src/businessLogic.js
+++ Teste de Sistemas/Exercicio teste unitario/02-calc-project/src/businessLogic.js
@@ -28,4 +28,19 @@ export function calculateAverage(purchases) {
     return divide(totalValue, purchases.length)
 }
 
-export function calculateDiscountedPrice(price, discountPercent) {}
+export function calculateDiscountedPrice(price, discountPercent) {
+    if(price <= 0){
+        throw new Error("O preço deve ser maior ou igual a zero!")
+    }
+
+    if(discountPercent < 0 || discountPercent > 100 ){
+        throw new Error("O desconto deve ser entre 0% e 100%");   
+    }
+    
+    const multiplyValue = multiply(price, discountPercent);
+    const discountValue = divide(multiplyValue, 100);
+    
+    return subtract(price, discountValue)
+}
+
+console.log(calculateDiscountedPrice(200, 10))
