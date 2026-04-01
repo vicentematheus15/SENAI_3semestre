0763a06b01c87088452501379c43a357d46884d1
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-03-31 21:28:12 -0300

    feat: implement salary increase calculation function in App component

--- Desenvolvimento Frontend/exercicios/salario/salario/src/App.tsx
+++ Desenvolvimento Frontend/exercicios/salario/salario/src/App.tsx
@@ -2,9 +2,23 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [salarioBase, setSalarioBase] = useState<string>('');
+  const [aumento, setAumento] = useState<string>('');
+  const [novoSalario, setNovoSalario] = useState<number>()
   
 
+  function calcularAumento(){
+    const sal = Number(salarioBase);
+    const a = Number(aumento)
+
+    const percentAumento = (sal * a) /100
+    const novoSal = sal + percentAumento;
+    return setNovoSalario(novoSal)
+  }
+
+  return(
+
+  )
 }
 
 export default App
