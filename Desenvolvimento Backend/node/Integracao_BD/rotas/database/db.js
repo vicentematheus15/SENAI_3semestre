cd3593c62de16b5ca6ed847dc1895886088e62eb
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-04-07 21:11:35 -0300

    feat: configure environment variables and update database connection settings

--- Desenvolvimento Backend/node/Integracao_BD/rotas/database/db.js
+++ Desenvolvimento Backend/node/Integracao_BD/rotas/database/db.js
@@ -1,12 +1,13 @@
 import pg from 'pg';
+
 const {Pool} = pg;
 
 const pool = new Pool({
-    host: 'localhost',
-    port: 5433,
-    user: 'matheusVicente',
-    password: '123',
-    database: 'clinica'
+    host: process.env.HOST_DB,
+    port: process.env.PORT_DB,
+    user: process.env.USER_DB,
+    password: process.env.PASSWORD_DB,
+    database: process.env.DATABASE_DB
 });
 
 export default pool;
--- Desenvolvimento Backend/node/Integracao_BD/rotas/index.js
+++ Desenvolvimento Backend/node/Integracao_BD/rotas/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import {routerContatos} from './router/contatosRouter.js'
+import 'dotenv/config';
 
 const app = express();
 app.use(express.json());
 
 app.use('/contatos', routerContatos);
 
-
-app.listen(3000, () => {
-    console.log('Aplicação rodando em: http://localhost:3000');
+const PORT = process.env.SERVER_PORT;
+app.listen(PORT, () => {
+    console.log('Aplicação rodando em: http://localhost:', PORT);
 });
