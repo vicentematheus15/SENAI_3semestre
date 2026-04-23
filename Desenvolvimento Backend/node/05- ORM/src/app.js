c413bdd86801148006ca1372e9db90d7dc90128f
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-04-22 21:24:27 -0300

    feat: initialize ORM project with Sequelize and PostgreSQL setup
    - created package.json with necessary dependencies including express, sequelize, and dotenv
    - implemented basic Express server in app.js with environment variable support
    - set up Sequelize connection in db.js for PostgreSQL database

---
+++ Desenvolvimento Backend/node/05- ORM/src/app.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import express from 'express';
+import sequelize from './database/db.js'
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.DB_PORT || 3000
+
+sequelize.sync({ alter: true }).then(() => {
+    app.listen(port, () =>{
+        console.log(`Servidor rodando em http://localhost:${port}`);
+    })
+})
+
