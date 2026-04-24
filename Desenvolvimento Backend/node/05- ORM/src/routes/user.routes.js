0858dcecb571b7b6aeab6951bc09d77b7089aeaa
Matheus Vicente <vicentematheus1507@gmail.com>
2026-04-24 12:36:57 -0300

    feat: implement JWT authentication in login and add auth middleware

--- Desenvolvimento Backend/node/05- ORM/src/routes/user.routes.js
+++ Desenvolvimento Backend/node/05- ORM/src/routes/user.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import * as controller from '../controllers/user.controller.js'
+import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.get('/', controller.getAllUsers);
-router.get('/:id', controller.getUserById)
+router.get('/:id', controller.getUserById);
 router.post('/create', controller.createUser);
+
 router.post('/login', controller.login);
-router.put('/:id', controller.updateUser)
-router.delete('/:id', controller.deleteUser)
+router.put('/:id', authenticate, controller.updateUser);
+router.delete('/:id', authenticate, controller.deleteUser);
 
 export default router;
