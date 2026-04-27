ba6ed864b459af9a3c0694cb1c62799ed0691568
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-04-27 20:54:58 -0300

    fix: correct import statements in app.js and database.js, and comment out unused route in auth.routes.js

--- Desenvolvimento Backend/SA/src/database/database.js
+++ Desenvolvimento Backend/SA/src/database/database.js
@@ -1,4 +1,4 @@
-import {Sequelize} from 'sequelize';
+import Sequelize from 'sequelize';
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
--- Desenvolvimento Backend/SA/src/routes/auth.routes.js
+++ Desenvolvimento Backend/SA/src/routes/auth.routes.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 
 const authRoutes = Router();
 
-authRoutes.post('/auth', );
+// authRoutes.post('/auth', );
 
 export default authRoutes;
