0bc2d5bdf54c4402dc71fe70b5653278d934af41
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-04-27 20:37:48 -0300

    feat: add authentication routes for user login

--- Desenvolvimento Backend/SA/src/app.js
+++ Desenvolvimento Backend/SA/src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import sequelize, { Sequelize } from './database/db.js';
 import express from 'express';
 import cors from 'cors';
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(cors({
 
 app.use(express.json());
 
-
+app.use('/auth', authRoutes)
 
 const port = process.env.API_PORT;
 
---
+++ Desenvolvimento Backend/SA/src/routes/auth.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const authRoutes = Router();
+
+authRoutes.post('/auth', );
+
+export default authRoutes;
