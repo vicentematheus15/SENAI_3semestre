c4973d5fa8d0db464246a63c03181a9d766d4dd9
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-04-28 19:59:24 -0300

    feat: enhance calcWinRate function and implement getRank and isElite functions for player ranking

--- Teste de Sistemas/TDD/src/playerUtils.js
+++ Teste de Sistemas/TDD/src/playerUtils.js
@@ -16,8 +16,50 @@ export function calcWinRate(wins, totalGames){
     if(totalGames === 0){
         throw new Error("O numero de jogos não pode ser zero.")        
     }
-    if(wins > totalGames || wins < 0){
-        throw new Error("O número de vitórias não pode ser negativo ou maior que o número de jogos.");
+    if(wins < 0 ||totalGames < 0 ){
+        throw new Error("O número de vitórias ou de jogos não podem ser negativos.");
+    }
+    if(wins > totalGames){
+        throw new Error("O número de vitórias não pode ser maior que o número de jogos..");
     }
     return (wins / totalGames) * 100
+}
+
+export function getRank(winRate){
+    if (typeof winRate !== 'number' ) {
+        throw new Error("O Winrate deve ser um número");
+    }
+    if(winRate <= 100 && winRate >= 75){
+        return 'Lendário'
+    }
+    if(winRate <= 74 && winRate >= 55){
+        return 'Diamante'
+    }
+    if(winRate <= 54 && winRate >= 40){
+        return 'Ouro'
+    }
+    if(winRate <= 39 && winRate >= 25){
+        return 'Prata'
+    }
+    if(winRate <= 24 && winRate >= 0){
+        return 'Bronze'
+    }
+}
+
+export function isElite(winRate){
+    if (typeof winRate !== 'number' ) {
+        throw new Error("O Winrate deve ser um número");
+    }
+
+    if(winRate <= 100 && winRate >= 75){
+        return true
+    }
+
+    if(winRate <= 74 && winRate >= 0){
+        return false
+    }
+}
+
+export function calcKDA(kills, deaths, assists){
+    
 }
