3b01ec6fde33e2f54e2596df154593b11b6b05b0
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-04-28 22:00:01 -0300

    feat: implement login function for user authentication with JWT

--- Desenvolvimento Backend/SA/src/routes/auth.routes.js
+++ Desenvolvimento Backend/SA/src/routes/auth.routes.js
@@ -4,5 +4,6 @@ import * as controller from '../controllers/auth.controller.js'
 const authRoutes = Router();
 
 authRoutes.post('/cadastro', controller.cadastrar);
+authRoutes.post('/login', controller.login);
 
 export default authRoutes;
