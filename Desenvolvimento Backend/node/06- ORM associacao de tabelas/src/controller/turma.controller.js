9cb54f94f98325a11b4b579578ceededd02ed6f7
Matheus Vicente <vicentematheus1507@gmail.com>
2026-05-07 17:47:37 -0300

    feat: enhance filtrarTurmas function to include totalAlunos count and improve query structure

--- Desenvolvimento Backend/node/06- ORM associacao de tabelas/src/controller/turma.controller.js
+++ Desenvolvimento Backend/node/06- ORM associacao de tabelas/src/controller/turma.controller.js
@@ -1,10 +1,39 @@
+import sequelize from "../database/database.js";
 import { Aluno, Turma } from "../models/index.js";
-import { Op } from "sequelize";
+import { Op, Sequelize } from "sequelize";
 
 export async function filtrarTurmas(req, res){
     try {
+        const turmasFiltradas = await Turma.findAll({
+            attributes: ['nome', 'semestre'], //essa linha não ta fazendo efeito
+            where: {
+                [Op.and]: [
+                    {
+                        [Op.or]: [
+                            { nome: { [Op.iLike]: '%EDM%' } }, 
+                            { nome: { [Op.iLike]: '%EDM%' } }
+                        ]
+                    }, {
+                        semestre: { [Op.between]: [ 1, 4 ] }
+                    }
+                ]
+            }, 
+            include: {
+                model: Aluno,
+                attributes: []
+            },
+            attributes: {
+                include: [
+                    [sequelize.fn('COUNT', sequelize.col('Alunos.id')), 'totalAlunos']
+                ]
+            },    
+            group: ['Turma.id']
+        });
+        console.log(`Turmas encontradas: ${turmasFiltradas.length}`);
         
+        return res.status(200).json(turmasFiltradas)
     } catch (error) {
-        
+        console.error(error);
+        return res.status(500).json({ erro: 'Erro interno do servidor' });
     }
 }
