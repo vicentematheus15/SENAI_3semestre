c418b7149c9b258f4a2cb81ac80f31f321d06981
Matheus Vicente <vicentematheus1507@gmail.com>
2026-05-08 10:29:01 -0300

    feat: update turma route to use aluno controller

--- Desenvolvimento Backend/node/06- ORM associacao de tabelas/src/routes/turma.routes.js
+++ Desenvolvimento Backend/node/06- ORM associacao de tabelas/src/routes/turma.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import * as controller from '../controller/turma.controller.js';
+import * as controller from '../controllers/aluno.controller.js';
 
 const turmaRoutes = Router();
 
