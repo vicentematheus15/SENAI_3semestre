e26a60052525f3fe6c75839dfd858bf29c1b768b
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-12 19:37:07 -0300

    feat: add initial convidados array and log output for original list

---
+++ Desenvolvimento Frontend/App map_filter_find_reduce/app.js
@@ -0,0 +1,9 @@
+const convidados = [
+  { id: 1, nome: "Lucas", sobrenome: "Silva", idade: 20 },
+  { id: 2, nome: "Maria", sobrenome: "Oliveira", idade: 17 },
+  { id: 3, nome: "João", sobrenome: "Santos", idade: 25 },
+  { id: 4, nome: "Ana", sobrenome: "Souza", idade: 16 }
+];
+
+console.log("===== LISTA ORIGINAL =====");
+console.log(convidados);
