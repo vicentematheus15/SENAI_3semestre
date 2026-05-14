f3924b8523652a8d973c152c41c2bd2a60a00b70
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-13 21:23:57 -0300

    feat: refactor App component to filter and display products by category

--- Desenvolvimento Frontend/App map_filter_find_reduce/exemplo crud-produtos/layout-react/src/App.jsx
+++ Desenvolvimento Frontend/App map_filter_find_reduce/exemplo crud-produtos/layout-react/src/App.jsx
@@ -5,116 +5,29 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const produtos = [
+  { id: 1, nome: "Notebook Dell Inspiron", categoria: "Eletrônicos", preco: 3499.90 },
+  { id: 2, nome: "Mouse Gamer Logitech", categoria: "Periféricos", preco: 189.90 },
+  { id: 3, nome: "Teclado Mecânico Redragon", categoria: "Periféricos", preco: 279.90 },
+  { id: 4, nome: "Monitor LG 24 Polegadas", categoria: "Monitores", preco: 899.90 },
+  { id: 5, nome: "Cadeira Gamer ThunderX3", categoria: "Acessórios", preco: 1299.90 },
+  { id: 6, nome: "Smartphone Samsung Galaxy", categoria: "Celulares", preco: 2499.90 },
+  { id: 7, nome: "Fone de Ouvido JBL", categoria: "Periféricos", preco: 299.90 },
+  { id: 8, nome: "HD Externo Seagate 1TB", categoria: "Armazenamento", preco: 379.90 },
+  { id: 9, nome: "Webcam Full HD Logitech", categoria: "Acessórios", preco: 249.90 },
+  { id: 10, nome: "Impressora HP DeskJet", categoria: "Acessórios", preco: 599.90 }
+];
 
+  const produtosFiltrados = produtos.filter((produto) => {
+    return produto.categoria === 'Periféricos'
+  })
+
+ 
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        
       </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
   )
 }
