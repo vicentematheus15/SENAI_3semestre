4c553352dcf8448a025af63502cd29c0182837ac
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-14 20:54:04 -0300

    primeiro teste e função

--- Teste de Sistemas/05-bdd-project/demo/src/authService.js
+++ Teste de Sistemas/05-bdd-project/demo/src/authService.js
@@ -1,1 +1,11 @@
 //  implemente aqui ao vivo
+export class AuthService {
+    constructor() {
+        this.users = []
+    }
+
+    register(email, senha){
+        this.users.push(email, senha)
+        return {success: true, user: {email}}
+    }
+}
--- Teste de Sistemas/05-bdd-project/demo/tests/authService.test.js
+++ Teste de Sistemas/05-bdd-project/demo/tests/authService.test.js
@@ -1,1 +1,27 @@
 //escreva os testes aqui ao vivo
+import { describe, expect, it, beforeEach } from 'vitest';
+import { AuthService } from '../src/authService';
+
+//Feature
+describe('Autenticação de Usuário', () => {
+    let authService 
+
+    beforeEach(() => {
+        authService = new AuthService()
+    })
+
+
+    //Scenario
+    it('Cadastro com dados válidos', () => {
+        //Given
+        const email = "maria@email.com"
+        const senha = "Senha123"
+
+        //When
+        const result = authService.register(email, senha)
+
+        //Then
+        expect(result.success).toBe(true)
+        expect(result.user.email).toBe(email)
+    })
+})
