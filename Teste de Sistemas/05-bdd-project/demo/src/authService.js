3b343c7c23f1e21f4b20e8c910c919bd0d836c46
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-14 21:25:02 -0300

    feat: mais 3 casos de testes e funções adicionados

--- Teste de Sistemas/05-bdd-project/demo/src/authService.js
+++ Teste de Sistemas/05-bdd-project/demo/src/authService.js
@@ -5,6 +5,19 @@ export class AuthService {
     }
 
     register(email, senha){
+        const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+        if(!emailRegex.test(email)) {
+            throw new Error("Email inválido");
+        }
+
+        if(senha.length < 6) {
+            throw new Error("Senha deve ter no mínimo 6 caracteres");
+        }
+
+        if(this.users.find(user => user.email === email )){
+            throw new Error("Email já cadastrado");
+        }
+
         this.users.push(email, senha)
         return {success: true, user: {email}}
     }
--- Teste de Sistemas/05-bdd-project/demo/tests/authService.test.js
+++ Teste de Sistemas/05-bdd-project/demo/tests/authService.test.js
@@ -23,5 +23,32 @@ describe('Autenticação de Usuário', () => {
         //Then
         expect(result.success).toBe(true)
         expect(result.user.email).toBe(email)
+    });
+
+    it('Cadastro com email inválido', () => {
+        //Given
+        const email = "email-invalido"
+        const senha = "Senha123"
+
+        //When + Then
+        expect(() => authService.register(email, senha)).toThrow("Email inválido")
+    });
+
+    it('Cadastro com senha muito curta', () => {
+        //Given
+        const email = "maria@email.com"
+        const senha = "123"
+
+        //When + Then
+        expect(() => authService.register(email, senha)).toThrow("Senha deve ter no mínimo 6 caracteres")
+    });
+
+    it('Cadastro com email já existente', () => {
+        //Given
+        authService.register('maria@email.com', 'Senha123')
+
+        //When + Then
+        expect(() => authService.register('maria@email.com', 'Senha123')).toThrow('Email já cadastrado')
     })
+
 })
