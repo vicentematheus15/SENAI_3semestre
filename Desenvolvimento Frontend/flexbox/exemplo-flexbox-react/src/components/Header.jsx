f76361eba1032d47e347ddaf0b11c05d575e799e
vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-20 20:11:22 -0300

    feat: implement Header component with navigation links

---
+++ Desenvolvimento Frontend/flexbox/exemplo-flexbox-react/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header className="menu">
+      <div className="logo">FlexReact</div>
+
+      <nav className="links">
+        <a href="#">Início</a>
+        <a href="#">Aulas</a>
+        <a href="#">Planos</a>
+        <a href="#">Contato</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
