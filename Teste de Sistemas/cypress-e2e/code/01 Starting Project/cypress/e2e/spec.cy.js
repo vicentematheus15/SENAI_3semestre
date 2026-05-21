8ea5267998cde80497c52d287667538917e02162
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-21 20:39:38 -0300

    feat: implement end-to-end test for task addition workflow

--- Teste de Sistemas/cypress-e2e/code/01 Starting Project/cypress/e2e/spec.cy.js
+++ Teste de Sistemas/cypress-e2e/code/01 Starting Project/cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('end-to-end', () => {
+  it('Fluxo completo de adição de task', () => {
     cy.visit('http://localhost:5173/')
+    cy.get('[data-cy="start-add-task-button"]').click()
+    cy.get(".modal").should("have.length", 1)
+    cy.get('#title').type("Task 1")
+    cy.get('#summary').type("Summary 1")
+    cy.get('[type="submit"]').click()
+    cy.get('.task').should("have.length", 1)
+
   })
 })
