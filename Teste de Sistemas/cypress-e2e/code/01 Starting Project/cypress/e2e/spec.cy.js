f3cc5248ddfddef5c08076eec88dd24fa4752060
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-21 21:08:24 -0300

    feat: add filter functionality for tasks by category

--- Teste de Sistemas/cypress-e2e/code/01 Starting Project/cypress/e2e/spec.cy.js
+++ Teste de Sistemas/cypress-e2e/code/01 Starting Project/cypress/e2e/spec.cy.js
@@ -47,4 +47,30 @@ describe('end-to-end', () => {
     cy.get('[type="submit"]').click()
     cy.get('.task').should("have.length", 3)
   })
+
+  it('Filtrar duas ou mais tarefas', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('[data-cy="start-add-task-button"]').click()
+    cy.get('#title').type("Task 1")
+    cy.get('#summary').type("Summary 1")
+    cy.get('[type="submit"]').click()
+
+    cy.get('[data-cy="start-add-task-button"]').click()
+    cy.get('#title').type("Task 2")
+    cy.get('#summary').type("Summary 2")
+    cy.get('#category').select("important")
+    cy.get('[type="submit"]').click()
+
+    cy.get('[data-cy="start-add-task-button"]').click()
+    cy.get('#title').type("Task 3")
+    cy.get('#summary').type("Summary 3")
+    cy.get('#category').select("important")
+    cy.get('[type="submit"]').click()
+
+    cy.get('#filter').select("important")
+    cy.get('.task').should("have.length", 2)
+    cy.get('.task').should("contain", "🔴")
+  })
+
+
 })
