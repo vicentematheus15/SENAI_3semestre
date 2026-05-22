50b123ce69b8bd41c1281b10e38bc37b87030c96
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-21 21:03:12 -0300

    feat: add test for adding a task with urgent category

--- Teste de Sistemas/cypress-e2e/code/01 Starting Project/cypress/e2e/spec.cy.js
+++ Teste de Sistemas/cypress-e2e/code/01 Starting Project/cypress/e2e/spec.cy.js
@@ -19,6 +19,16 @@ describe('end-to-end', () => {
 
   })
 
+  it('Adicionar uma tarefa com categoria urgente', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('[data-cy="start-add-task-button"]').click()
+    cy.get('#title').type("Task 1")
+    cy.get('#summary').type("Summary 1")
+    cy.get('#category').select("urgent")
+    cy.get('[type="submit"]').click()
+    cy.get('.task').should("contain", "🚨")
+  })
+  
   it('Adicionar duas ou mais tarefas', () => {
     cy.visit('http://localhost:5173/')
     cy.get('[data-cy="start-add-task-button"]').click()
