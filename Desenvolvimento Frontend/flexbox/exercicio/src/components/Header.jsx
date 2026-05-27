b0fb0195239495a8c4354323560ef0de7bfb1894
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-05-26 21:15:16 -0300

    feat: implement Header component and integrate it into App

---
+++ Desenvolvimento Frontend/flexbox/exercicio/src/components/Header.jsx
@@ -0,0 +1,33 @@
+function Header() {
+    return(
+        <header>
+            <div>
+                <nav>
+                    <a href="#">Início</a>
+                    <a href="#">Sobre</a>
+                    <a href="#">Cursos</a>
+                    <a href="#">Planos</a>
+                    <a href="#">Contato</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
+export default Header
+
+// function Header() {
+//   return (
+//     <header className="menu">
+//       <div className="logo">FlexReact</div>
+
+//       <nav className="links">
+//         <a href="#">Início</a>
+//         <a href="#">Aulas</a>
+//         <a href="#">Planos</a>
+//         <a href="#">Contato</a>
+//       </nav>
+//     </header>
+//   );
+// }
+
+// export default Header;
