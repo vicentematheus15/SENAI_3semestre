2f179b9fecda656a4e78c839c8c2c6d5fc330f32
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-06-18 20:59:44 -0300

    feat: enhance order submission tests and enable badge visibility check for new orders

--- Teste de Sistemas/cypress/03-cypress_dashboard/cypress/e2e/orders.cy.js
+++ Teste de Sistemas/cypress/03-cypress_dashboard/cypress/e2e/orders.cy.js
@@ -22,12 +22,17 @@ afterEach(() => {
 it('Deve adicionar um novo pedido ao submeter o formualrio', () =>{
     cy.fillOrderForm('Carlos Souza', 'Monitor 27', '550')
     cy.get('[data-testid="submit-order"]').click()
-
+    cy.get('[data-testid="orders-list"]').first().should('contain', 'Carlos Souza')
+    cy.get('[data-testid="orders-list"]').first().should('contain', 'Monitor 27')
+    cy.get('[data-testid="orders-list"]').first().should('contain', '550')
 })
 
-// it("Deve exibir o badge Pendente para pedidos novos", () => {
-
-// })
+it("Deve exibir o badge Pendente para pedidos novos", () => {
+    cy.fillOrderForm('Joao Pedro', 'Mouse gamer', '200')
+    cy.get('[data-testid="submit-order"]').click()
+    cy.get('[data-testid="order-row-4"] > :nth-child(5) > .badge').should('be.visible')
+    
+})
 
 // it("Deve filtrar pedidos por status", () => {
 
