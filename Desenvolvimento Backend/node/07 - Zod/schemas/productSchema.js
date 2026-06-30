e8b1d08e5b191d2cd8c52c63af01ce62ee515e30
Vicentematheus15 <vicentematheus1507@gmail.com>
2026-06-29 21:44:59 -0300

    fix: adjust formatting in product schema definition

--- Desenvolvimento Backend/node/07 - Zod/schemas/productSchema.js
+++ Desenvolvimento Backend/node/07 - Zod/schemas/productSchema.js
@@ -1,6 +1,6 @@
 import {z} from "zod";
 
- const productSchema = z.object({
+const productSchema = z.object({
 nome: z.string().min(2, "deve ter no minimo 2 caracteres"),
 categoria:z.enum(["ferramentas", "materiais", "tintas", "eletronicos", "EPIs"]),
 estoque:z.number(),
